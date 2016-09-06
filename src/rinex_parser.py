a18ddb1e1c80acdf11d5dbb00b241101586314b4
root <root@rhe-garu.prod.lan>
2016-09-06 16:41:45 +1000

    Moving to model of parseFile() -> parseFilename() + (use type from name to get) MetObj | NavObj | ObsObj == subclass of RINEXFile

--- src/rinex_parser.py
+++ src/rinex_parser.py
@@ -1,8 +1,10 @@
 import datetime
 import os
-from rinex_header import *
+from parsec import *
+from collections import defaultdict
 
-class RINEXFile():
+class RINEX():
+
     def __init__(self, data, filename=None):
         """Initialize RINEXData object
 
@@ -42,4 +44,81 @@ class RINEXFile():
         if not eoh:
             raise InvalidHeader('Missing END OF HEADER field')
 
-        self.header = RINEXHeader(''.join([header, eoh]))
+        self.header = header + eoh
+
+        self._parseHeader()
+
+
+    def _parseHeader(self):
+        header_fields = defaultdict(str)
+        for line in self.header.splitlines():
+            field, label = line[:60], trim_whitespace(line[60:])
+            header_fields[label] += field
+
+        header_fields = dict(header_fields)
+
+	self.version, self.type, satellite = Parser(joint(
+            n_ANY(9).parsecmap(float),
+            compose(n_ANY(11), one_of('MON')),
+            compose(n_ANY(19), n_ANY(1)))).parse(
+                header_fields['RINEX VERSION / TYPE'])
+
+
+class Meteorological(RINEX):
+    def __init__(self, file):
+        super(Meteorological, self).__init__()
+
+class Navigational(RINEX):
+    def __init__(self, file):
+        super(Navigational, self).__init__()
+
+class Observational(RINEX):
+    def __init__(self, file):
+        super(Observational, self).__init__()
+
+
+def parseRINEX(data, filename=None):
+    pass
+
+
+# Custom parser generators
+def n_A(n):
+    """Given n, return Parser for string of length n, containing only alphanumeric
+    characters
+    """
+    return regex('([A-Za-z0-9]){{{0}}}'.format(n))
+
+def n_I(n):
+    """ Given n, return parser for an integer between one and n digits long
+    """
+    return regex('([0-9]){{1,{0}}}'.format(n)).parsecmap(int)
+
+def n_ANY(n):
+    """Given n, return Parser for string of n length containing any characters
+
+    Useful for ignoring n characters with the compose >> or skip << operations
+    or for capturing fixed length fields
+    """
+    return regex('(.){{{0}}}'.format(n))
+
+def n_A3(n):
+    """Given n, return parser for space seperated list of n A3's
+
+    A3 is a token for three successive alphanumeric characters
+    """
+    return count(compose(spaces(), n_A(3)), n)
+
+# Lambda funcion to remove excess whitespace from a string
+trim_whitespace = lambda a: ' '.join(a.split())
+
+
+class InvalidHeader(Exception):
+    """Indicates that the RINEX header is syntactically incorrect in some way
+    """
+
+class RINEXHeaderFieldError(ValueError):
+    """Used to indicate that a RINEX header field was of the wrong data type
+
+    Example:
+        If a string is found where a float was expected
+    """
