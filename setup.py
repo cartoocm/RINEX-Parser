8c0d18ae7e3f452fe09728da09a4997dd23c6f2e
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2016-10-17 03:09:22 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='rinex_parser',
+    name='RINEXer',
     version='1.0',
     description='Python module for parsing metadata from RINEX data files',
     author='Brandon Owen',
-    author_email='brandon.owen@hotmail.com'
+    author_email='brandon.owen@hotmail.com',
+    packages=['rinexer']
 )
