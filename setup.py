f713bb6d381bd3ec82885d32240a4f3369ea1267
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2016-10-17 02:50:37 +0000

    Changed setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version='1.0',
     description='Python module for parsing metadata from RINEX data files',
     author='Brandon Owen',
-    author_email='brandon.owen@hotmail.com'
+    author_email='brandon.owen@hotmail.com',
+    packages=['rinex_parser']
 )
